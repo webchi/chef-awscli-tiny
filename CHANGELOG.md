f27b20dbdb87ba1c6fefb97321ce91385dfb2dbc
Vasiliy Shakhunov <v.shakhunov@gmail.com>
2019-08-27 15:44:30 +0300

    Update 1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 This file is used to list changes made in each version of the awscli-tiny cookbook.
 
+# 0.1.2
+
+- Move to python 3 support
+- Remove unnecessary dependecies 
+- Remove centos support
+
 # 0.1.0
 
 Initial release.
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # Cookbook:: awscli-tiny
 # Recipe:: default
@@ -7,60 +9,60 @@
 uname = node['awscli']['user']
 home_dir = '/home/' + uname
 
-python_runtime '2' do
-    provider :system
-    pip_version false
-    setuptools_version false
-    virtualenv_version false
-    wheel_version false
+package 'Install Python' do
+  package_name 'python3-minimal'
 end
 
 remote_file "#{home_dir}/get-pip.py" do
-    source 'https://bootstrap.pypa.io/get-pip.py'
-    owner uname
-    group uname
-    mode '0755'
+  source 'https://bootstrap.pypa.io/get-pip.py'
+  owner uname
+  group uname
+  mode '0755'
 end
 
 execute 'install pip' do
-    command "sudo -u #{uname} -H sh -c 'python #{home_dir}/get-pip.py --user'"
-    not_if "test -e #{home_dir}/.local/bin/pip"
+  command "sudo -u #{uname} -H sh -c 'python3 #{home_dir}/get-pip.py --user'"
+  not_if "test -e #{home_dir}/.local/bin/pip3"
 end
 
 execute 'add bashrc' do
-    user uname
-    command "sudo -u #{uname} -H sh -c 'touch #{home_dir}/.bashrc'"
-    not_if "test -e #{home_dir}/.bashrc"
+  user uname
+  command "sudo -u #{uname} -H sh -c 'touch #{home_dir}/.bashrc'"
+  not_if "test -e #{home_dir}/.bashrc"
 end
 
 execute 'add local path' do
-    user uname
-    command "sudo -u #{uname} -H sh -c 'echo export PATH=#{home_dir}/.local/bin:$PATH' >> #{home_dir}/.bashrc"
-    not_if "cat #{home_dir}/.bashrc | grep 'export PATH=#{home_dir}/.local/bin'"
+  user uname
+  command "sudo -u #{uname} -H sh -c 'echo export PATH=#{home_dir}/.local/bin:$PATH' >> #{home_dir}/.bashrc"
+  not_if "cat #{home_dir}/.bashrc | grep 'export PATH=#{home_dir}/.local/bin'"
 end
 
 execute 'install aws' do
-    command "sudo -u #{uname} -H sh -c '#{home_dir}/.local/bin/pip install awscli --upgrade --user'"
-    not_if "test -e #{home_dir}/.local/bin/aws"
+  command "sudo -u #{uname} -H sh -c '#{home_dir}/.local/bin/pip3 install awscli --upgrade --user'"
+  not_if "test -e #{home_dir}/.local/bin/aws"
 end
 
 directory "#{home_dir}/.aws" do
-    owner uname
-    group uname
-    mode '0755'
-    action :create
+  owner uname
+  group uname
+  mode '0755'
+  action :create
 end
 
 template "#{home_dir}/.aws/config" do
-    source "config.erb"
-    owner uname
-    group uname
-    mode '0600'
+  source 'config.erb'
+  owner uname
+  group uname
+  mode '0600'
 end
 
 template "#{home_dir}/.aws/credentials" do
-    source "credentials.erb"
-    owner uname
-    group uname
-    mode '0600'
+  source 'credentials.erb'
+  owner uname
+  group uname
+  mode '0600'
+end
+
+file "#{home_dir}/get-pip.py" do
+  action :delete
 end
